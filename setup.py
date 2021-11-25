4547421a610255b32455ccf9a386cdffd0ad55bf
Henry Krumb <mail@henrykrumb.de>
2021-11-25 22:04:05 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+
+setup(
+    name='training_notifications',
+    version='1.0',
+    description='',
+    author='Henry Krumb',
+    author_email='mail@henrykrumb.de',
+    url='',
+    packages=['training_notifications'],
+)
