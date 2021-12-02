712e8b54053c4f5d166b201cbae301b8cfffe0c8
Henry Krumb <mail@henrykrumb.de>
2021-12-02 13:27:17 +0100

    fix missing recipient in XMPP notifier

--- training_notifications/notifier_xmpp.py
+++ training_notifications/notifier_xmpp.py
@@ -5,19 +5,20 @@ from .notifier import Notifier
 
 
 class NotifierXMPP(Notifier):
-    def __init__(self, jid, password):
+    def __init__(self, jid, password, recipient):
         super(self, NotifierXMPP).__init__()
         self.client = aioxmpp.PresenceManagedClient(
             jid,
             aioxmpp.make_security_layer(password)
         )
+        self.recipient = recipient
 
     async def notify(self, epoch, metrics):
         title = Notifier.make_title()
         message = title + '\n' + Notifier.make_message(epoch, metrics)
         async with client.connected() as stream:
             msg = aioxmpp.Message(
-                to=recipient_jid,
+                to=recipient,
                 type_=aioxmpp.MessageType.CHAT,
             )
             msg.body[None] = message
