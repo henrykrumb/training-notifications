b326bb2a7c0be5d7b74f056b5b193882b30d4488
Henry Krumb <mail@henrykrumb.de>
2021-12-02 11:49:15 +0100

    improve error handling and documentation

--- training_notifications/training_notifications.py
+++ training_notifications/training_notifications.py
@@ -1,31 +1,42 @@
 import os
 import json
+import traceback
+
+import click
 
 
 def notifier_from_dict(d):
+    """
+    Create notifier from dict.
+    Use this method to configure your notifiers before training.
+
+    :param d: dict containing notifier configuration
+    """
     module_name = 'notifier'
     if 'notifier' in d:
-        module_name += '_' + d.get('notifier').lower()
-    module = __import__(module_name)
+        module_name += '_' + d['notifier'].lower()
+    try:
+        module = __import__(module_name)
+    except ImportError:
+        click.secho(f'Error: Notifier {module_name} not found', fg='red')
+        exit()
     class_name = 'Notifier' + d.get('notifier')
     klass = getattr(module, class_name)
     instance = klass(**d)
     return instance
 
 
-def notifier_from_json(filename):
-    with open(filename, 'r') as f:
-        d = json.load(f)
-    return notifier_from_dict(d)
-
-
 def notify_all(notifiers, epoch, metrics):
     """
-    Send notifications using a list of notifiers.
+    Send notifications utilizing a list of notifiers.
+
+    :param notifiers: List of Notifier subclassed objects
+    :param epoch: Current epoch
+    :param metrics: Dict of metrics (floats) to report
     """
     for notifier in notifiers:
         try:
             notifier.notify(epoch, metrics)
-        except Exception as ex:
-            print('Warning: Failed to notify about training progress.')
-            print(ex)
+        except Exception:
+            click.secho('Warning: Failed to notify about training progress.', fg='red')
+            traceback.print_exc()
