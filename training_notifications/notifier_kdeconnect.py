95e93a42aedce9ea7c4862293e75d1feb364294d
Henry Krumb <mail@henrykrumb.de>
2021-12-02 13:20:55 +0100

    fix id encoding in kdeconnect notifier

--- training_notifications/notifier_kdeconnect.py
+++ training_notifications/notifier_kdeconnect.py
@@ -5,10 +5,11 @@ from .notifier import Notifier
 
 class NotifierKDEConnect(Notifier):
     def __init__(self, device_id=''):
+        super(NotifierKDEConnect, self).__init__()
         if not device_id:
             cmd = 'kdeconnect-cli -a --id-only'.split(' ')
             out = subprocess.run(cmd, capture_output=True)
-            self.device_id = out.stdout.encode('utf-8')
+            self.device_id = out.stdout.decode('utf-8').strip()
         else:
             self.device_id = device_id
 
@@ -16,5 +17,5 @@ class NotifierKDEConnect(Notifier):
         title = Notifier.make_title()
         message = Notifier.make_message(epoch, metrics)
         cmd = f'kdeconnect-cli -d {self.device_id} --ping-msg'.split(' ')
-        cmd.append(f'"{title}: {message}"')
+        cmd.append(f'"{title}:\n{message}"')
         subprocess.run(cmd)
